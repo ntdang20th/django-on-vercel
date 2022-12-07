a509777c3dd34a17966fc4aad0af7a33e851476c
Dang <ntdang_20th@student.agu.edu.vn>
2022-12-07 15:32:12 +0700

    retry

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ echo "Collect static..."
 python3 manage.py collectstatic --noinput --clear
 
 echo "Create superuser..."
-try:
+try
   python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', '123')"
-except:
+catch
   print("Create failure!")
