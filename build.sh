f6fb177c438dd6120f7c5606433ef12c2bd4f964
Dang <ntdang_20th@student.agu.edu.vn>
2022-12-09 16:09:33 +0700

    wtf

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ echo "Building project..."
 python3 -m pip install -r requirements.txt
 
 echo "DAPHNEEEEEEEEEEEE..."
-daphne fallingdetection.asgi:application --bind 0.0.0.0 -v2
+daphne backend.asgi:application --bind 0.0.0.0 -v2
 python3 manage.py runworker channel_layer -v2
 
 echo "Make migrations..."
