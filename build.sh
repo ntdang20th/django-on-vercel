aeb340c159471af8801145fd82e07e327f2a164e
Dang <ntdang_20th@student.agu.edu.vn>
2022-12-09 15:44:07 +0700

    haha

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 echo "Building project..."
 python3 -m pip install -r requirements.txt
 
+echo "DAPHNEEEEEEEEEEEE..."
+daphne -b 0.0.0.0 -p 8001 backend.asgi:application
+
 echo "Make migrations..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
@@ -9,5 +12,3 @@ python3 manage.py migrate --noinput
 echo "Collect static..."
 python3 manage.py collectstatic --noinput --clear
 
-echo "DAPHNEEEEEEEEEEEE..."
-daphne -b 0.0.0.0 -p 8001 backend.asgi:application
