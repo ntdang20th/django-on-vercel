2479eaf3ab1cc862c18411f1f46fc781ee731faa
Dang <ntdang_20th@student.agu.edu.vn>
2022-12-09 16:58:13 +0700

    fds

--- build.sh
+++ build.sh
@@ -2,11 +2,6 @@
 echo "Building project..."
 python3 -m pip install -r requirements.txt
 
-echo "DAPHNEEEEEEEEEEEE..."
-python3 manage.py runworker channel_layer -v2
-daphne backend.asgi:application --bind 0.0.0.0 -v2
-
-
 echo "Make migrations..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
@@ -14,3 +9,5 @@ python3 manage.py migrate --noinput
 echo "Collect static..."
 python3 manage.py collectstatic --noinput --clear
 
+echo "DAPHNEEEEEEEEEEEE..."
+daphne -b 0.0.0.0 -p 8001 backend.asgi:application
