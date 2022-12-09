531619b726d3d69651d43ee242a9984f3d6a6f7e
Dang <ntdang_20th@student.agu.edu.vn>
2022-12-09 16:53:36 +0700

    kkk

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ echo "Building project..."
 python3 -m pip install -r requirements.txt
 
 echo "DAPHNEEEEEEEEEEEE..."
-daphne backend.asgi:application --bind 0.0.0.0 -v2
 python3 manage.py runworker channel_layer -v2
+daphne backend.asgi:application --bind 0.0.0.0 -v2
+
 
 echo "Make migrations..."
 python3 manage.py makemigrations --noinput
