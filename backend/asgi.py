541a1d45637794c78efed3424d873357c65f59bd
Dang <ntdang_20th@student.agu.edu.vn>
2022-12-09 15:52:06 +0700

    hehe

--- backend/asgi.py
+++ backend/asgi.py
@@ -8,20 +8,28 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 import os
 import django
+from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from django.urls import path
-
-import backend.asgi
 from api.consumers import SensorConsumer
 
+django_asgi_app = get_asgi_application()
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
-application = get_asgi_application()
 ws_patterns = [
     path('ws/sensor/', SensorConsumer.as_asgi()),
 ]
 application = ProtocolTypeRouter({
-    'websocket': URLRouter(ws_patterns)
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(ws_patterns)
+        )
+    ),
 })
 app = application
