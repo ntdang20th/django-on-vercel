314e1fe02d75399f3582e2e53141abdea83fa98f
Dang <ntdang_20th@student.agu.edu.vn>
2022-12-09 15:42:51 +0700

    djaksdl

--- backend/asgi.py
+++ backend/asgi.py
@@ -7,17 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 import os
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import path
+
+import backend.asgi
 from api.consumers import SensorConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
 application = get_asgi_application()
 ws_patterns = [
     path('ws/sensor/', SensorConsumer.as_asgi()),
 ]
 application = ProtocolTypeRouter({
+    "http": backend.asgi.application,
     'websocket': URLRouter(ws_patterns)
 })
 app = application
