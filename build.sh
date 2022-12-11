388040423e1659944a58c110f8a28bcdde0c8697
Dang <ntdang_20th@student.agu.edu.vn>
2022-12-11 16:19:42 +0700

    subprocess

--- build.sh
+++ build.sh
@@ -11,5 +11,6 @@ python3 manage.py collectstatic --noinput --clear
 
 echo "DAPHNEEEEEEEEEEEE..."
 #command: daphne -b 0.0.0.0 -p 8001 backend.asgi:application
-from daphne.cli import CommandLineInterface
-CommandLineInterface().run(('backend.asgi:application', '--bind', '0.0.0.0', '--port', '8001'))
+#from daphne.cli import CommandLineInterface
+#CommandLineInterface().run(('backend.asgi:application', '--bind', '0.0.0.0', '--port', '8001'))
+subprocess.run(["daphne", "-b 0.0.0.0 -p 8001 backend.asgi:application"])
