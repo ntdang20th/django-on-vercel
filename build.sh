cdfe251b8a2ed5a1bb2910b82fad02fa054408c7
Dang <ntdang_20th@student.agu.edu.vn>
2022-12-11 16:12:29 +0700

    command

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ echo "Collect static..."
 python3 manage.py collectstatic --noinput --clear
 
 echo "DAPHNEEEEEEEEEEEE..."
-daphne -b 0.0.0.0 -p 8001 backend.asgi:application
+command: daphne -b 0.0.0.0 -p 8001 backend.asgi:application
