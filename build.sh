17868465c6e7805b64bb25554f09d91f580bb7e1
Dang <ntdang_20th@student.agu.edu.vn>
2022-12-11 16:21:40 +0700

    import:

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #build the project
+from daphne.cli import CommandLineInterface
+
+
 echo "Building project..."
 python3 -m pip install -r requirements.txt
 
@@ -11,6 +14,4 @@ python3 manage.py collectstatic --noinput --clear
 
 echo "DAPHNEEEEEEEEEEEE..."
 #command: daphne -b 0.0.0.0 -p 8001 backend.asgi:application
-#from daphne.cli import CommandLineInterface
-#CommandLineInterface().run(('backend.asgi:application', '--bind', '0.0.0.0', '--port', '8001'))
-subprocess.run(["daphne", "-b 0.0.0.0 -p 8001 backend.asgi:application"])
+CommandLineInterface().run(('backend.asgi:application', '--bind', '0.0.0.0', '--port', '8001'))
